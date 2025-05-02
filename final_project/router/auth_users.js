8d1715979dd64d6057af459208a604816265dda7
Michael Avoyan <64565419+michaelavoyan@users.noreply.github.com>
2025-05-02 17:11:39 +0300

    Task 7: logging in as a registered user

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,20 +3,33 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [{username: "michael.avoyan", password: "123qwe"},];
 
 const isValid = (username)=>{
   return true;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  return users.some(user => user.username === username && user.password === password);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required." });
+  }
+
+  if (!authenticatedUser(username, password)) {
+    return res.status(401).json({ message: "Invalid credentials." });
+  }
+
+  const accessToken = jwt.sign({ username: username }, 'access', { expiresIn: '1h' });
+
+  req.session.authorization = "Bearer " + accessToken;
+
+  return res.status(200).json({ message: "User successfully logged in", token: accessToken });
 });
 
 // Add a book review
