ff2677373c22f5e6617201f978a71cc8b946ce3b
Michael Avoyan <64565419+michaelavoyan@users.noreply.github.com>
2025-05-02 19:27:24 +0300

    Task 10: change to promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,7 +26,15 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(200).json({books: books});
+    new Promise((resolve, reject) => {
+        resolve(books);
+    })
+        .then((data) => {
+            return res.status(200).json({ books: data });
+        })
+        .catch((err) => {
+            return res.status(500).json({ message: "Unable to fetch books" });
+        });
 });
 
 // Get book details based on ISBN
