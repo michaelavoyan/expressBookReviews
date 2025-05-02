1d9e2fea631a2e4bcd7ff5811da9edfc35cc998d
Michael Avoyan <64565419+michaelavoyan@users.noreply.github.com>
2025-05-02 19:43:39 +0300

    Task 14: Completed all tasks from 1 to 13 for Book Review project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -95,7 +95,7 @@ public_users.get('/title/:title', function (req, res) {
             return res.status(404).json({ message: error });
         });
 });
-
+ 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   const isbn = req.params.isbn;
