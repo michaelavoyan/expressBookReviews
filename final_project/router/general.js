22e2d1a5a590ece7ae94da59da4d5990f7b5d5d5
Michael Avoyan <64565419+michaelavoyan@users.noreply.github.com>
2025-05-02 15:55:21 +0300

    getting the list of books available in the shop

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,7 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json({books: books});
 });
 
 // Get book details based on ISBN
@@ -21,7 +20,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
  });
-  
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
